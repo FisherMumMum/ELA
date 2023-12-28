fa8d7fbe95daf2bc356955fd61b9637732816b4d
Duy Trương Ngọc <ngoc250604@gmail.com>
2023-12-28 20:01:12 +0700

    new changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')()
+require('dotenv').config()
 app = express
+hostname = process.env.HOST_NAME 
+port = process.env.PORT || 8080
 app.set('views','./')
 app.set('view engine','ejs')
 app.get('/',(reg ,res)=>{
     // res.send('<h1>Chao cac ban</h1>')
     res.render('sample')
 })
-app.listen(8080,()=>{
+app.listen(port,hostname,()=>{
     console.log('Running')
 })
