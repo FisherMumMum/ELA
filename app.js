5aec4a5e645f94fde8ea5b364dfe93321cb82cee
bradtraversy <ngoc250604@gmail.com>
2023-12-29 07:26:58 +0700

    new changes

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express')()
+const path = require('path')
+const exp = require('express')()
+const express = require('express')
 require('dotenv').config()
-app = express
+app = exp
 hostname = process.env.HOST_NAME 
 port = process.env.PORT || 8080
 app.set('views','./')
 app.set('view engine','ejs')
+app.use('/public', express.static(path.join(__dirname, '/public')))
 app.get('/',(reg ,res)=>{
     // res.send('<h1>Chao cac ban</h1>')
     res.render('sample')
 })
 app.listen(port,hostname,()=>{
     console.log('Running')
+    console.log(__dirname)
 })
